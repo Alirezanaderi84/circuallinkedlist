c3bc0d7c416fc5bd730700104914b7ab251457f4
Alirezanaderi84 <148426222+Alirezanaderi84@users.noreply.github.com>
2024-12-08 21:59:16 +0330

    Add files via upload

---
+++ CircularLinkedList/CircularLinkedList/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace CircularLinkedList
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.addlist = new System.Windows.Forms.Button();
+            this.removelist = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.addfirst = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // addlist
+            // 
+            this.addlist.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(178)))), ((int)(((byte)(178)))));
+            this.addlist.Font = new System.Drawing.Font("B Titr", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.addlist.ForeColor = System.Drawing.SystemColors.Control;
+            this.addlist.Location = new System.Drawing.Point(126, 200);
+            this.addlist.Name = "addlist";
+            this.addlist.Size = new System.Drawing.Size(75, 33);
+            this.addlist.TabIndex = 0;
+            this.addlist.Text = "افزودن";
+            this.addlist.UseVisualStyleBackColor = false;
+            this.addlist.Click += new System.EventHandler(this.addlist_Click);
+            // 
+            // removelist
+            // 
+            this.removelist.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(178)))), ((int)(((byte)(178)))));
+            this.removelist.Font = new System.Drawing.Font("B Titr", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.removelist.ForeColor = System.Drawing.SystemColors.Control;
+            this.removelist.Location = new System.Drawing.Point(126, 254);
+            this.removelist.Name = "removelist";
+            this.removelist.Size = new System.Drawing.Size(75, 33);
+            this.removelist.TabIndex = 1;
+            this.removelist.Text = "حذف";
+            this.removelist.UseVisualStyleBackColor = false;
+            this.removelist.Click += new System.EventHandler(this.removelist_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(81, 116);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(305, 45);
+            this.textBox1.TabIndex = 2;
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(241)))), ((int)(((byte)(240)))), ((int)(((byte)(232)))));
+            this.listBox1.Font = new System.Drawing.Font("Microsoft New Tai Lue", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 21;
+            this.listBox1.Location = new System.Drawing.Point(422, 55);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(202, 340);
+            this.listBox1.TabIndex = 3;
+            // 
+            // addfirst
+            // 
+            this.addfirst.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(122)))), ((int)(((byte)(178)))), ((int)(((byte)(178)))));
+            this.addfirst.Font = new System.Drawing.Font("B Titr", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.addfirst.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.addfirst.Location = new System.Drawing.Point(126, 200);
+            this.addfirst.Name = "addfirst";
+            this.addfirst.Size = new System.Drawing.Size(75, 33);
+            this.addfirst.TabIndex = 4;
+            this.addfirst.Text = "افزودن";
+            this.addfirst.UseVisualStyleBackColor = false;
+            this.addfirst.Click += new System.EventHandler(this.addfirst_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("B Titr", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label1.Location = new System.Drawing.Point(461, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(163, 33);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "لیست پیوندی چرخشی";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("B Titr", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.label2.Location = new System.Drawing.Point(160, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(147, 23);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "عبارت مورد نظر را وارد کنید";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Font = new System.Drawing.Font("B Titr", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.checkBox1.Location = new System.Drawing.Point(248, 192);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(135, 27);
+            this.checkBox1.TabIndex = 10;
+            this.checkBox1.Text = "افزودن به ابتدای لیست";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged_1);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Font = new System.Drawing.Font("B Titr", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(178)));
+            this.checkBox2.Location = new System.Drawing.Point(248, 216);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(133, 27);
+            this.checkBox2.TabIndex = 11;
+            this.checkBox2.Text = "افزودن به انتهای لیست";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(220)))), ((int)(((byte)(242)))), ((int)(((byte)(241)))));
+            this.ClientSize = new System.Drawing.Size(648, 422);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.addfirst);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.removelist);
+            this.Controls.Add(this.addlist);
+            this.MinimumSize = new System.Drawing.Size(664, 461);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button addlist;
+        private System.Windows.Forms.Button removelist;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button addfirst;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ CircularLinkedList/CircularLinkedList/Form1.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CircularLinkedList
+{
+    public partial class Form1 : Form
+    {
+        CircularLinkedList<string> circularList = new CircularLinkedList<string>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void addlist_Click(object sender, EventArgs e)
+        {
+
+            if (textBox1.Text.Trim() == "")
+            {
+                MessageBox.Show("عبارت را وارد کنید");
+            }
+            else
+            {
+                circularList.Add(textBox1.Text.ToString());
+                circularList.PrintList(listBox1);
+            }
+
+        }
+
+        private void removelist_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text.Trim() == "")
+            {
+                MessageBox.Show("عبارت را وارد کنید");
+            }
+            else
+            {
+                circularList.remove(textBox1.Text.ToString()); 
+                circularList.PrintList(listBox1);
+            }
+        }
+        private void addfirst_Click(object sender, EventArgs e)
+        {
+            circularList.Addtofirst(textBox1.Text.ToString());
+            circularList.PrintList(listBox1);
+        }
+        private void checkBox1_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked) {
+                addfirst.Show();
+                addlist.Hide();
+                checkBox2.Checked = false;
+            }
+            else
+            {
+                checkBox2.Checked = true;
+            }
+            
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            checkBox1.Checked = true;
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                checkBox1.Checked = false;
+                addfirst.Hide();
+                addlist.Show();
+            }
+            else
+            {
+                checkBox1.Checked = true;
+            }
+        }
+
+    }
+}
+   public class CircularLinkedList<T>
+    {
+         LinkedList<T> list = new LinkedList<T>();
+         Dictionary<LinkedListNode<T>, LinkedListNode<T>> nextNodes= new Dictionary<LinkedListNode<T>, LinkedListNode<T>>();
+        public void Add(T data)
+        {
+            var newNode = new LinkedListNode<T>(data);
+
+            if (list.Count == 0)
+            {
+                list.AddFirst(newNode);
+                nextNodes[newNode] = newNode; 
+            }
+            else
+            {
+                var lastNode = list.Last;
+                list.AddLast(newNode);
+                nextNodes[lastNode] = newNode; 
+                nextNodes[newNode] = list.First; 
+            }
+        }
+    public void Addtofirst(T data)
+    {
+        var newNode = new LinkedListNode<T>(data);
+        if (list.Count == 0)
+        {
+            list.AddFirst(newNode);
+            nextNodes[newNode] = newNode;
+        }
+        else
+        {
+            var firstNode = list.First;
+            list.AddFirst(newNode);
+            nextNodes[newNode] = firstNode;
+            var lastNode = list.Last;
+            nextNodes[lastNode] = newNode;
+        }
+    }
+    
+        public void remove(T data)
+        {
+        
+            var removeNode = list.Find(data);
+            if (removeNode != null)
+            {
+                if (list.Count == 1)
+                {
+                    list.Remove(removeNode);
+                    nextNodes.Clear();
+                }
+                else
+                {
+                    var prevNode = GetPreviousNode(removeNode);
+                    nextNodes[prevNode] = nextNodes[removeNode]; 
+                    list.Remove(removeNode);
+                    nextNodes.Remove(removeNode);
+                }
+            }
+        else
+        {
+            MessageBox.Show("در لیست موجود نیست");
+        }
+        }
+        private LinkedListNode<T> GetPreviousNode(LinkedListNode<T> node)
+        {
+            var current = list.First;
+            while (nextNodes[current] != node)
+            { current = nextNodes[current]; }
+            return current;
+        }
+
+
+    public void PrintList(ListBox listBox)
+    {
+        listBox.Items.Clear();
+        if (list.Count == 0)
+        {
+            return;
+        }
+
+        var current = list.First;
+        foreach (T item in list)
+        {
+            listBox.Items.Add(current.Value);
+            current = nextNodes[current];
+        
+        }
+    }  
+    
+    
+   }
+
+
+    
